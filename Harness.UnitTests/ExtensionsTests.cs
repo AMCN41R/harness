cb3ae995af3ea44021cee828eb7a635acd3015c4
Alex McNair <alex.mcnair@hotmail.co.uk>
2016-01-09 16:26:18 +0000

    Added Extensions tests

---
+++ Harness.UnitTests/ExtensionsTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Xunit;
+using Harness.Attributes;
+
+namespace Harness.UnitTests
+{
+    public class ExtensionsTests
+    {
+        [Fact]
+        public void GetAttribute_CallOnClasWithCustomAttribute_RetunrsTheAttribute()
+        {
+            // Arrange
+            var testClass = new ExtensionsTestClass();
+
+            // Act
+            var result = 
+                testClass.GetType().GetAttribute<MongoIntegrationTestClass>();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<MongoIntegrationTestClass>(result);
+
+        }
+
+        [Fact]
+        public void GetAttribute_CallOnClassWithoutAttribute_ReturnsNull()
+        {
+            // Arrange
+            var testClass = new ExtensionTestClassWithoutAttribute();
+
+            // Act
+            var result =
+                testClass.GetType().GetAttribute<MongoIntegrationTestClass>();
+
+            // Assert
+            Assert.Null(result);
+
+        }
+
+        [Fact]
+        public void GetAttribute_CallOnClassWithDifferentAtribte_ReturnsNull()
+        {
+            // Arrange
+            var testClass = new ExtensionTestClassWithDifferentAttribute();
+
+            // Act
+            var result =
+                testClass.GetType().GetAttribute<MongoIntegrationTestClass>();
+
+            // Assert
+            Assert.Null(result);
+
+        }
+
+        [MongoIntegrationTestClass]
+        private class ExtensionsTestClass
+        {
+        }
+
+        private class ExtensionTestClassWithoutAttribute
+        {
+        }
+
+        [TestAttribute]
+        private class ExtensionTestClassWithDifferentAttribute
+        {
+        }
+
+        private class TestAttribute : Attribute
+        {
+        }
+
+    }
+}
