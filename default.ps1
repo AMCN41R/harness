bd6fdf89cd81b457ed9bf1c800f5be045e2feabe
Alex McNair <alex.mcnair@hotmail.co.uk>
2020-03-29 21:46:02 +0100

    Added v2.1 nuget package - updated nuspec and csproj to use license tag over deprecated licenseUrl - updated psake build scripts

--- default.ps1
+++ default.ps1
@@ -2,51 +2,51 @@
 task default -depends Clean, Build, Docs
 
 task Clean {
-    "Deleting /bin contents..."
-    Get-ChildItem -inc bin -rec | Remove-Item -rec -force
-    "Deleting /bin contents...DONE"
+  "Deleting /bin contents..."
+  Get-ChildItem -inc bin -rec | Remove-Item -rec -force
+  "Deleting /bin contents...DONE"
 
-    "Deleting /obj contents..."
-    Get-ChildItem -inc obj -rec | Remove-Item -rec -force
-    "Deleting /obj contents...DONE"
+  "Deleting /obj contents..."
+  Get-ChildItem -inc obj -rec | Remove-Item -rec -force
+  "Deleting /obj contents...DONE"
 
-    "Deleting Old Test Results..."
-    Get-ChildItem -inc TestResults -rec | Remove-Item -rec -force
-    "Deleting Old Test Results...DONE"
+  "Deleting Old Test Results..."
+  Get-ChildItem -inc TestResults -rec | Remove-Item -rec -force
+  "Deleting Old Test Results...DONE"
 }
 
 task Build {
-    dotnet build .\src\Harness.sln -c Release
+  dotnet build .\src\Harness.sln -c Release
 }
 
 task Test {
-    $testdlls =
-        ".\src\Harness.Tests.Unit\bin\Release\netcoreapp2.0\Harness.Tests.Unit.dll",
-        ".\src\Harness.Tests.Integration\bin\Release\netcoreapp2.0\Harness.Tests.Integration.dll"
+  $testdlls =
+  ".\src\Harness.Tests.Unit\bin\Release\netcoreapp2.0\Harness.Tests.Unit.dll",
+  ".\src\Harness.Tests.Integration\bin\Release\netcoreapp2.0\Harness.Tests.Integration.dll"
 
-    dotnet vstest  $testdlls --Parallel --logger:trx
+  dotnet vstest  $testdlls --Parallel --logger:trx
 }
 
 task Docs {
-    "Generating documentation..."
-    .\tools\docfx\docfx .\src\Harness.Docs\docfx_project\docfx.json
-    "Generating documentation...DONE"
+  "Generating documentation..."
+  .\tools\docfx\docfx .\src\Harness.Docs\docfx_project\docfx.json
+  "Generating documentation...DONE"
 
-    "Cleaning old docs folder..."
-    Get-ChildItem -Path .\Docs\ -Recurse | Remove-Item -Recurse -Force
-    "Cleaning old docs folder...DONE"
+  "Cleaning old docs folder..."
+  Get-ChildItem -Path .\Docs\ -Recurse | Remove-Item -Recurse -Force
+  "Cleaning old docs folder...DONE"
 
-    "Copying to docs to root..."
-    Copy-Item ".\src\Harness.Docs\docfx_project\_site\*" -Destination ".\docs\" -Recurse
-    "Copying docs to root...DONE"
+  "Copying to docs to root..."
+  Copy-Item ".\src\Harness.Docs\docfx_project\_site\*" -Destination ".\docs\" -Recurse
+  "Copying docs to root...DONE"
 }
 
 task Pack {
-    "Packing deployables..."
-    dotnet pack .\src\Harness\ --configuration Release --output ..\..\tools\nuget\versions
+  "Packing deployables..."
+  dotnet pack .\src\Harness\ --configuration Release --output .\tools\nuget\versions
 
-    if ($lastexitcode -ne 0) {
-        throw "Packing task failed! Build failed."
-    }
-    "Packing deployables...DONE"
+  if ($lastexitcode -ne 0) {
+    throw "Packing task failed! Build failed."
+  }
+  "Packing deployables...DONE"
 }
